cd6931ddec7e3ed98ac020ab168c2af0d4775705
Nora Gaughan <noragaughan@gmail.com>
2020-06-10 22:24:24 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -10,6 +10,18 @@ def valid_move?(board, position)
 
 end
 
+def valid_move?(board, position)
+	spot = position.to_i - 1 
+	if !position_taken?(board, spot) && spot.between?(0,8)
+		false 
+
+	elsif 
+		false
+
+	end
+
+end
+
 def position_taken?(board, position)
   if board[position] == " " || board[position] == "" || board[position] == nil
   	false
